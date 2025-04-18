11cd281515f28efc9dc51ea435f43a0b092b6440
kindaCacti <m.wawrzyniaczek@gmail.com>
2025-04-18 15:01:47 +0200

    moved stuff to different folders

---
+++ backend/src/test_create_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "test_table" (
+  "id" integer NOT NULL,
+  "value" integer NOT NULL,
+  PRIMARY KEY(id)
+);
+
+INSERT INTO test_table VALUES(1, 100);
+INSERT INTO test_table VALUES(2, 200);
+INSERT INTO test_table VALUES(3, 300);
+INSERT INTO test_table VALUES(4, 400);
