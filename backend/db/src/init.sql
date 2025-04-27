ac3998677420f99fa0b14033bdb5995791b71280
kindaCacti <m.wawrzyniaczek@gmail.com>
2025-04-27 13:16:17 +0200

    moved files around

--- backend/db/src/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "test_table" (
-  "id" integer NOT NULL,
-  "value" integer NOT NULL
-);
-
-INSERT INTO test_table VALUES (1, 100);
-INSERT INTO test_table VALUES (2, 200);
-INSERT INTO test_table VALUES (3, 300);
