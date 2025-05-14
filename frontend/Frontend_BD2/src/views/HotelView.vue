3cc5fe1579ea8685399c6755a19112cc7919f623
mattigu <gulinski.mateusz1@gmail.com>
2025-05-14 13:33:16 +0200

    Merged hotel list

--- frontend/Frontend_BD2/src/views/HotelView.vue
+++ frontend/Frontend_BD2/src/views/HotelView.vue
@@ -1,16 +1,15 @@
 <script setup>
 
+// View that displays a detailed Hotel page.
+
 defineProps({
-	id: String
+	id: {
+		type: String,
+		required: true
+	},
 })
 
 </script>
-
 <template>
-  <p>Specific hotel view with id= {{ id }}
-    Here a hotel page with the id can be loaded after the user clicks it when searching</p>
-
-
-  <a href="https://router.vuejs.org/guide/advanced/data-fetching.html">Exact example from docs what to do</a>
-
+  <h1> Detailed hotel page</h1>
 </template>
