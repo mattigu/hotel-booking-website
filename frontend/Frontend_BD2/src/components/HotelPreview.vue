0aad6e1dcd0ec29c5e8c4650964c3c48d4ec8e7e
Natalia Dudzińska <01187218@pw.edu.pl>
2025-05-14 13:04:50 +0200

    added hotelPrieview and get hotels

---
+++ frontend/Frontend_BD2/src/components/HotelPreview.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+	name: {
+		type: String,
+		required: true
+	},
+	id: {
+		type: String,
+		required: true
+	}
+})
+
+function test() {
+	console.log("something")
+	return
+}
+
+let text = async () => {text.value = await test();}
+
+</script>
+<template>
+  <div class="hotel">
+    <h1> {{ name }} HOTEL ID: {{ id }} </h1>
+    <button @click="text()">aaaa</button>
+  </div>
+</template>
+
+<style>
+.hotel {
+	background-color: rgb(172, 221, 221);
+	width: 70%;
+}
+
+</style>
--- frontend/Frontend_BD2/src/views/HotelBrowse.vue
+++ frontend/Frontend_BD2/src/views/HotelBrowse.vue
@@ -1,5 +1,48 @@
 <script setup>
+import { ref } from 'vue'
+import HotelPreview from '@/components/HotelPreview.vue';
+
+defineProps({
+	name: {
+		type: String,
+		required: true
+	},
+	id: {
+		type: Number,
+		required: true
+	}
+})
+// hotels/getall
+async function getHotels() {
+	const request = new Request("http://localhost:3000/hotels/getall", {
+		method: "GET",
+	});
+
+	try {
+		const response = await fetch(request)
+		if (!response.ok) {
+			throw new Error(`Failed to get response`);}
+		let hotels = await response.json();
+		return hotels
+	} catch (error) {
+		console.error(error.message);
+		alert("Failed")
+		return;
+	}
+}
+
+let allHotels = ref([]);
+const loadHotels = async () => {allHotels.value = await getHotels();}
+loadHotels();
 
 </script>
 
-<template> Hotel browse view</template>
+<template>
+  <button @click="loadHotels() "> Find hotels </button>
+  <template v-for="hotel in allHotels">
+    <HotelPreview
+      :name="hotel.name"
+      :id="hotel.id"
+    />
+  </template>
+</template>
