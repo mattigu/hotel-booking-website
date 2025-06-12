85d2abadd508f9aec790ad20bca498c42a27d5a6
kindaCacti <m.wawrzyniaczek@gmail.com>
2025-06-12 14:29:24 +0200

    removed hotel directory

--- backend/src/hotel/hotel.go
+++
@@ -1,9 +0,0 @@
-package hotel
-
-type Hotel struct {
-	Id           int    `json:"id"`
-	Name         string `json:"name"`
-	AddressId    int    `json:"adress_id"`
-	Description  string `json:"description"`
-	StarStandard int    `json:"star_standard"`
-}
