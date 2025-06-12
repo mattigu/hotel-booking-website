56a03a5583457b81f13fb49ef64a4a9774cba6fe
Natalia Dudzińska <01187218@pw.edu.pl>
2025-06-12 19:40:07 +0200

    added basic hotel page view to HotelView

--- frontend/Frontend_BD2/src/views/HotelView.vue
+++ frontend/Frontend_BD2/src/views/HotelView.vue
@@ -34,10 +34,37 @@ const hotel = ref(null)
 const loadHotel = async (id) => {hotel.value = await fetchHotel(id)}
 loadHotel(props.id)
 
+
 </script>
+
 <template>
   <div v-if="hotel" class="content">
-    <h2>Hello from {{ hotel.name }} page id = {{ hotel.id }}</h2>
-    <p>{{ hotel.description }}</p>
+    <div class="hotel_top">
+      <p>name: {{ hotel.name }} page id = {{ hotel.id }}</p>
+      <!-- will change this to img later -->
+      <p>Hotel star standard: {{ hotel.star_standard }}</p>
+      <h2>{{ hotel.name }}, {{ hotel.description }}, {{ hotel.address['city'] }}</h2>
+      <p>{{ hotel.address['street'] }} {{ hotel.address["house_number"] }}, {{ hotel.address["city"] }}, {{ hotel.address["country"] }}</p>
+    </div>
+    <div class="hotel_img">
+      <img :src="`{{ hotel.photo_url }}`" onerror="this.onerror=null;src=`https://u.profitroom.com/2018-focushotels-pl/thumb/1920x1080/uploads/DJI_0372_MID.jpg`">
+    </div>
+    <div class="amenities_list">
+      <h2>Amenities</h2>
+      <template v-for="amenity in hotel.amenities" :key="amenity.id">
+        <p>{{ amenity.name }}</p>
+        <p>- {{ amenity.description }}</p>
+      </template>
+    </div>
   </div>
 </template>
+
+<style>
+.hotel_img img{
+	object-fit: cover;
+    height: 240px;
+    width: 240px;
+    border-radius: 5%;
+	aspect-ratio: 1;
+}
+</style>
