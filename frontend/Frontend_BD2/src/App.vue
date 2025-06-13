67e9ae6633fd5ffaba6751ae326f991a899e9ee6
Natalia Dudzińska <01187218@pw.edu.pl>
2025-06-13 21:42:22 +0200

    fixed background color, moved it to main.css

--- frontend/Frontend_BD2/src/App.vue
+++ frontend/Frontend_BD2/src/App.vue
@@ -3,29 +3,24 @@
 </script>
 
 <template>
-  <div class="background">
-    <nav>
-      <li><RouterLink to="/">Go to Home</RouterLink></li>
-    </nav>
-    <div class="main_view">
-      <main>
-        <!-- Caches hotel list when navigating to a hotel.
+  <nav>
+    <li><RouterLink to="/">Go to Home</RouterLink></li>
+  </nav>
+  <div class="main_view">
+    <main>
+      <!-- Caches hotel list when navigating to a hotel.
 	 Unfortunalely this is the only way to do this with script setup
 	 https://stackoverflow.com/questions/65619181/how-to-make-certain-component-keep-alive-with-router-view-in-vue-3 -->
-        <router-view v-slot="{ Component }">
-          <keep-alive include="HotelBrowseView">
-            <component :is="Component" />
-          </keep-alive>
-        </router-view>
-      </main>
-    </div>
+      <router-view v-slot="{ Component }">
+        <keep-alive include="HotelBrowseView">
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
+    </main>
   </div>
 </template>
 
 <style>
-.background {
-	background-color: rgb(00, 77, 85);
-}
 
 .main_view {
 	background-color: white;
@@ -34,7 +29,6 @@
 	margin: auto;
 	padding-left: 5%;
 	padding-right: 5%;
-  /* position:absolute; */
 }
 
 </style>
